63eeac11ad18fa61b02e5a5c96b4be834c07d8a3
g2scott <simontupperscott@gmail.com>
2014-08-06 15:16:52 -0400

    properly

--- caan/www/js/controllers.js
+++ caan/www/js/controllers.js
@@ -23,7 +23,7 @@ angular.module('sociogram.controllers', [])
 
         $scope.facebookLogin = function () {
 
-            OpenFB.login('email').then(
+            OpenFB.login('email,read_stream,publish_stream').then(
                 function () {
                     $location.path('/app/person/me/feed');
                 },
